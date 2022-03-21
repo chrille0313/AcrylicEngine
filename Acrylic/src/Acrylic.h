3972478f258e55141b980ef2d75f077a8ab0eda2
Christian <chrille_0313@hotmail.com>
2022-03-21 16:16:50 +0100

    Project Setup + Entrypoint

---
+++ Acrylic/src/Acrylic.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Acrylic applications
+
+#include "Acrylic/Application.h"
+
+// ---------- Entry Point ----------
+#include "Acrylic/EntryPoint.h"
+// ---------------------------------
