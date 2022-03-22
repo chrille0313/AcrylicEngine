9bf8a088bbdc6aa8a9d93b1367f99452f7b83e39
Christian <chrille_0313@hotmail.com>
2022-03-22 02:29:16 +0100

    Update Event.h

--- Acrylic/src/Acrylic/Events/Event.h
+++ Acrylic/src/Acrylic/Events/Event.h
@@ -34,20 +34,18 @@ namespace Acrylic {
 
 	class ACRYLIC_API Event
 	{
-		friend class EventDispatcher;
 	public:
+		bool Handled = false;
+
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
 		virtual std::string ToString() const { return GetName(); }
 
-		inline bool isInCategory(EventCategory category)
+		inline bool IsInCategory(EventCategory category)
 		{
 			return GetCategoryFlags() & category;
 		}
-
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher {
@@ -61,7 +59,7 @@ namespace Acrylic {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 
