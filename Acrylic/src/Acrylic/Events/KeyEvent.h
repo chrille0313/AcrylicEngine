6f5fa4c516aeab066c37eb04b77ad505f5157b9b
Christian <chrille_0313@hotmail.com>
2022-03-22 01:32:16 +0100

    Update KeyEvent.h

--- Acrylic/src/Acrylic/Events/KeyEvent.h
+++ Acrylic/src/Acrylic/Events/KeyEvent.h
@@ -28,7 +28,7 @@ namespace Acrylic {
 		std::string ToString() const override
 		{
 			std::stringstream ss;
-			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats";
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
 			return ss.str();
 		}
 
@@ -41,7 +41,7 @@ namespace Acrylic {
 	class ACRYLIC_API KeyReleasedEvent : public KeyEvent
 	{
 	public:
-		KeyReleasedEvent(int keycode, int repeatCount) : KeyEvent(keycode) {}
+		KeyReleasedEvent(int keycode) : KeyEvent(keycode) {}
 
 		std::string ToString() const override
 		{
