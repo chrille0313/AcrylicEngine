d0d27747aefd80cfbae12ee601bee04c5b0278cb
Christian <chrille_0313@hotmail.com>
2022-03-22 01:44:51 +0100

    Refactor

--- Acrylic/src/Acrylic/Log.h
+++ Acrylic/src/Acrylic/Log.h
@@ -23,15 +23,15 @@ namespace Acrylic {
 }
 
 // Core log macros
-#define AC_CORE_TRACE(...)		::Acrylic::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define AC_CORE_INFO(...)		::Acrylic::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define AC_CORE_WARN(...)		::Acrylic::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define AC_CORE_ERROR(...)		::Acrylic::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define AC_CORE_FATAL(...)		::Acrylic::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define AC_CORE_TRACE(...)	::Acrylic::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define AC_CORE_INFO(...)	::Acrylic::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define AC_CORE_WARN(...)	::Acrylic::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define AC_CORE_ERROR(...)	::Acrylic::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define AC_CORE_FATAL(...)	::Acrylic::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 // Client log macros
-#define AC_CLIENT_TRACE(...)	::Acrylic::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define AC_CLIENT_INFO(...)		::Acrylic::Log::GetClientLogger()->info(__VA_ARGS__)
-#define AC_CLIENT_WARN(...)		::Acrylic::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define AC_CLIENT_ERROR(...)	::Acrylic::Log::GetClientLogger()->error(__VA_ARGS__)
-#define AC_CLIENT_FATAL(...)	::Acrylic::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define AC_TRACE(...)		::Acrylic::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define AC_INFO(...)		::Acrylic::Log::GetClientLogger()->info(__VA_ARGS__)
+#define AC_WARN(...)		::Acrylic::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define AC_ERROR(...)		::Acrylic::Log::GetClientLogger()->error(__VA_ARGS__)
+#define AC_FATAL(...)		::Acrylic::Log::GetClientLogger()->fatal(__VA_ARGS__)
