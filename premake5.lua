26c23b3738733606ef9d6f0dfe08715d68b3827c
Christian <chrille_0313@hotmail.com>
2022-05-22 21:25:59 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,10 @@ workspace "Acrylic"
 		"Dist",
 	}
 
+	flags {
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
