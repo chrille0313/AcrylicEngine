c0de9951fd401749e64077574d2a5d5fd7663566
Christian <chrille_0313@hotmail.com>
2022-05-22 21:28:57 +0200

    Update LayerStack.cpp

--- Acrylic/src/Acrylic/Core/LayerStack.cpp
+++ Acrylic/src/Acrylic/Core/LayerStack.cpp
@@ -8,8 +8,10 @@ namespace Acrylic {
 
 	LayerStack::~LayerStack()
 	{
-		for (Layer* layer : m_Layers)
+		for (Layer* layer : m_Layers) {
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
