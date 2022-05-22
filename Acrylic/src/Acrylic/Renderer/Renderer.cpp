572e4194857d94a1d24de3297937937170c6b32a
Christian <74824121+christian-mattsson-nti-johanneberg@users.noreply.github.com>
2022-05-22 14:04:10 +0200

    Created Refs and Scopes

--- Acrylic/src/Acrylic/Renderer/Renderer.cpp
+++ Acrylic/src/Acrylic/Renderer/Renderer.cpp
@@ -1,7 +1,9 @@
 #include "acpch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
 
+ 
 namespace Acrylic {
 
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -16,10 +18,11 @@ namespace Acrylic {
 
 	}
 
-	void Renderer::Submit(const std::shared_ptr<VertexArray>& vertexArray, const std::shared_ptr<Shader>& shader)
+	void Renderer::Submit(const Ref<VertexArray>& vertexArray, const Ref<Shader>& shader, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
