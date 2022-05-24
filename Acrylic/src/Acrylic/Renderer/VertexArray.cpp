19c9654cdee528f953a7e25d1c62172fd278004c
Christian <chrille_0313@hotmail.com>
2022-05-25 01:22:49 +0200

    Removed Raw Pointer

--- Acrylic/src/Acrylic/Renderer/VertexArray.cpp
+++ Acrylic/src/Acrylic/Renderer/VertexArray.cpp
@@ -8,14 +8,14 @@
 
 namespace Acrylic {
 
-	VertexArray* VertexArray::Create()
+	Ref<VertexArray> VertexArray::Create()
 	{
 		switch (Renderer::GetAPI()) {
 			case RendererAPI::API::None:
 				AC_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 				return nullptr;
 			case RendererAPI::API::OpenGL:
-				return new OpenGLVertexArray();
+				return std::make_shared<OpenGLVertexArray>();
 		}
 
 		AC_CORE_ASSERT(false, "Unknown RendererAPI!");
