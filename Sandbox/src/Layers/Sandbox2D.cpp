27fd8ca52ba66797619a51557c1005c976c28c9b
Christian <chrille_0313@hotmail.com>
2022-05-29 16:25:29 +0200

    Added textures

--- Sandbox/src/Layers/Sandbox2D.cpp
+++ Sandbox/src/Layers/Sandbox2D.cpp
@@ -4,7 +4,15 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
+static const uint32_t s_MapWidth = 24;
+static const char* s_MapTiles =
+"AAAAAAAAAAAAAAAAAAAAAAAA"
+"AAAAAAAAAAAAAASSAAAAAAAA"
+"AAAAAAAAAAAAAAAAAAAAAAAA"
+"GGGGGGGGGGGGGGGGGGGGGGGG"
+"DDDDDDDDDDDDDDDDDDDDDDDD"
+"DDDDDDDDDDDDDDDDDDDDDDDD"
+;
 
 
 Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_MainCameraController(1280.0f / 720.0f, true)
@@ -16,6 +24,35 @@ void Sandbox2D::OnAttach()
 	AC_PROFILE_FUNCTION();
 
 	m_CheckerboardTexture = Acrylic::Texture2D::Create("assets/textures/Checkerboard.png");
+
+	// PixelPlatformer
+	m_SpriteSheetTiles = Acrylic::Texture2D::Create("Game/assets/PixelPlatformer/Sprites/Tilemap/tiles_packed.png");
+	m_SpriteSheetBackgrounds = Acrylic::Texture2D::Create("Game/assets/PixelPlatformer/Sprites/Tilemap/tilemap_packed.png");
+
+	m_MapWidth = s_MapWidth;
+	m_MapHeight = strlen(s_MapTiles) / s_MapWidth;
+
+	m_TextureEmpty = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetBackgrounds, { 2, 0 }, { 24, 24 });
+
+	// Air
+	m_TextureAir = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetBackgrounds, { 0, 1 }, { 24, 24 });
+
+	// Texture
+	m_TextureGrass = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 0, 7 }, { 18, 18 });		// Grass
+	m_TextureGrass01 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 1, 7 }, { 18, 18 });	// Grass01
+	m_TextureGrass02 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 2, 7 }, { 18, 18 });	// Grass02
+	m_TextureGrass03 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 3, 7 }, { 18, 18 });	// Grass03
+
+	// Dirt
+	m_TextureDirt01 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 1, 2 }, { 18, 18 });
+	m_TextureDirt02 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 2, 2 }, { 18, 18 });
+	m_TextureDirt03 = Acrylic::SubTexture2D::CreateFromCoords(m_SpriteSheetTiles, { 3, 2 }, { 18, 18 });
+
+
+	// Map textures
+	s_TextureMap['G'] = m_TextureGrass02;
+	s_TextureMap['D'] = m_TextureDirt02;
+	s_TextureMap['A'] = m_TextureAir;
 }
 
 void Sandbox2D::OnDetach()
@@ -26,6 +63,8 @@ void Sandbox2D::OnDetach()
 void Sandbox2D::OnUpdate(Acrylic::Timestep ts)
 {
 	AC_PROFILE_FUNCTION();
+
+	AC_TRACE("{0} fps: {1}", ts, ts.GetFps());
 
 	// Update
 
@@ -45,25 +84,30 @@ void Sandbox2D::OnUpdate(Acrylic::Timestep ts)
 	{
 		AC_PROFILE_SCOPE("Renderer Draw");
 
-
 		Acrylic::Renderer2D::BeginScene(m_MainCameraController.GetCamera());
 
-		Acrylic::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-		Acrylic::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.0f, 0.0f, 45.0f }, { 0.5f, 1.0f }, { 0.3f, 0.5f, 0.6f, 1.0f });
-		Acrylic::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 0.0f, 0.0f, 0.0f }, { 5.0f, 5.0f }, m_CheckerboardTexture, { 1.0f, 1.0f, 1.0f, 1.0f }, 5.0f);
+		//Acrylic::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 0.0f, 0.0f, 0.0f }, { 5.0f, 5.0f }, m_CheckerboardTexture, { 1.0f, 1.0f, 1.0f, 1.0f }, 5.0f);
 
-		Acrylic::Renderer2D::EndScene();
+		for (uint32_t y = 0; y < m_MapHeight; y++) {
+			for (uint32_t x = 0; x < m_MapWidth; x++) {
+				char tileType = s_MapTiles[x + y * m_MapWidth];
+				Acrylic::Ref<Acrylic::SubTexture2D> texture;
 
+				if (s_TextureMap.find(tileType) != s_TextureMap.end()) {
+					texture = s_TextureMap[tileType];
+				}
+				else {
+					texture = m_TextureEmpty;
+				}
 
-		Acrylic::Renderer2D::BeginScene(m_MainCameraController.GetCamera());
-		for (float y = -5.0f; y < 5.0f; y += 0.5f) {
-			for (float x = -5.0f; x < 5.0f; x += 0.5f) {
-				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
-				Acrylic::Renderer2D::DrawQuad({ x, y }, glm::vec3(0.0f), { 0.45f, 0.45f }, color);
+				Acrylic::Renderer2D::DrawQuad({ x - s_MapWidth / 2.0f,  m_MapHeight - y - m_MapHeight / 2.0f, 0.5f }, { 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, texture);
 			}
 		}
-		Acrylic::Renderer2D::EndScene();
 
+		Acrylic::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureGrass02);
+		//Acrylic::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureDirt01);
+
+		Acrylic::Renderer2D::EndScene();
 	}
 
 }
--- Sandbox/src/Layers/Sandbox2D.h
+++ Sandbox/src/Layers/Sandbox2D.h
@@ -19,8 +19,26 @@ public:
 private:
 	Acrylic::OrthographicCameraController m_MainCameraController;
 
-
 	Acrylic::Ref<Acrylic::Texture2D> m_CheckerboardTexture;
+	Acrylic::Ref<Acrylic::Texture2D> m_SpriteSheetTiles;
+	Acrylic::Ref<Acrylic::Texture2D> m_SpriteSheetBackgrounds;
 
-	//glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+
+	// Background
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureEmpty;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureAir;
+
+	// Grass
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureGrass;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureGrass01;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureGrass02;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureGrass03;
+
+	// Dirt
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureDirt01;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureDirt02;
+	Acrylic::Ref<Acrylic::SubTexture2D> m_TextureDirt03;
+
+	uint32_t m_MapWidth, m_MapHeight;
+	std::unordered_map<char, Acrylic::Ref<Acrylic::SubTexture2D>> s_TextureMap;
 };
