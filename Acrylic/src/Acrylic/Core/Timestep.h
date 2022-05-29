69390636574fefcd69530905a7d3fa1814f15797
Christian <chrille_0313@hotmail.com>
2022-05-29 16:27:44 +0200

    Update Timestep.h

--- Acrylic/src/Acrylic/Core/Timestep.h
+++ Acrylic/src/Acrylic/Core/Timestep.h
@@ -12,6 +12,7 @@ namespace Acrylic {
 
 		float GetSeconds() const { return m_Time; }
 		float GetMilliseconds() const { return m_Time * 1000.0f; }
+		float GetFps() const { return 1 / m_Time; }
 	private:
 		float m_Time;
 	};
