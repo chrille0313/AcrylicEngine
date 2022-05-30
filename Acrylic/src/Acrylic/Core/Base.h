63a5050291645adbcd7e2c4bdf310cf94a781605
Christian <chrille_0313@hotmail.com>
2022-05-30 19:54:22 +0200

    Update Base.h

--- Acrylic/src/Acrylic/Core/Base.h
+++ Acrylic/src/Acrylic/Core/Base.h
@@ -85,6 +85,7 @@
 
 #define AC_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
+
 namespace Acrylic {
 
 	template<typename T>
