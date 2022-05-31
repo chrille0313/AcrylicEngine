c20d84799e798fadf0a8335cd4d90a313d2639db
Christian <chrille_0313@hotmail.com>
2022-06-01 00:31:26 +0200

    Updated SceneCamera

--- Acrylic/src/Acrylic/Scene/SceneCamera.cpp
+++ Acrylic/src/Acrylic/Scene/SceneCamera.cpp
@@ -11,20 +11,6 @@ namespace Acrylic {
 		RecalculateProjection();
 	}
 
-	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
-	{
-		float oldSize = m_OrthographicSize;
-		float oldNear = m_OrthographicNear;
-		float oldFar = m_OrthographicFar;
-
-		m_OrthographicSize = size;
-		m_OrthographicNear = nearClip;
-		m_OrthographicFar = farClip;
-
-		if (m_OrthographicSize != oldSize || m_OrthographicNear != oldNear || m_OrthographicFar != oldFar)
-			RecalculateProjection();
-	}
-
 	void SceneCamera::SetViewportSize(uint32_t width, uint32_t height)
 	{
 		float oldAspectRatio = m_AspectRatio;
@@ -34,14 +20,49 @@ namespace Acrylic {
 			RecalculateProjection();
 	}
 
+	void SceneCamera::SetPerspective(float fov, float nearClip, float farClip)
+	{
+		float oldFOV = m_PerspectiveFOV;
+		float oldNear = m_PerspectiveNear;
+		float oldFar = m_PerspectiveFar;
+
+		m_ProjectionType = ProjectionType::Perspective;
+		m_PerspectiveFOV = fov;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
+
+		if (m_PerspectiveFOV != oldFOV || m_PerspectiveNear != oldNear || m_PerspectiveFar != oldFar)
+			RecalculateProjection();
+	}
+
+	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
+	{
+		float oldSize = m_OrthographicSize;
+		float oldNear = m_OrthographicNear;
+		float oldFar = m_OrthographicFar;
+
+		m_ProjectionType = ProjectionType::Orthographic;
+		m_OrthographicSize = size;
+		m_OrthographicNear = nearClip;
+		m_OrthographicFar = farClip;
+
+		if (m_OrthographicSize != oldSize || m_OrthographicNear != oldNear || m_OrthographicFar != oldFar)
+			RecalculateProjection();
+	}
+
 	void SceneCamera::RecalculateProjection()
 	{
-		float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoBottom = -m_OrthographicSize * 0.5f;
-		float orthoTop = m_OrthographicSize * 0.5f;
+		if (m_ProjectionType == ProjectionType::Perspective) {
+			m_ViewProjection = glm::perspective(glm::radians(m_PerspectiveFOV), m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		}
+		else if (m_ProjectionType == ProjectionType::Orthographic) {
+			float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoBottom = -m_OrthographicSize * 0.5f;
+			float orthoTop = m_OrthographicSize * 0.5f;
 
-		m_ViewProjection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+			m_ViewProjection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		}
 	}
 
 }
--- Acrylic/src/Acrylic/Scene/SceneCamera.h
+++ Acrylic/src/Acrylic/Scene/SceneCamera.h
@@ -8,19 +8,47 @@ namespace Acrylic {
 	class SceneCamera : public Camera
 	{
 	public:
+		enum class ProjectionType { Perspective = 0, Orthographic = 1 };
+
+	public:
 		SceneCamera();
 		virtual ~SceneCamera() = default;
 
-		void SetOrthographic(float size, float nearClip, float farClip);
 		void SetViewportSize(uint32_t width, uint32_t height);
 
+		void SetPerspective(float fov, float nearClip, float farClip);
+		float GetPerspectiveFOV() const { return m_PerspectiveFOV; }
+		void SetPerspectiveFOV(float fov) { m_PerspectiveFOV = fov; RecalculateProjection(); }
+		float GetPerspectiveNear() const { return m_PerspectiveNear; }
+		void SetPerspectiveNear(float nearClip) { m_PerspectiveNear = nearClip; RecalculateProjection(); }
+		float GetPerspectiveFar() const { return m_PerspectiveFar; }
+		void SetPerspectiveFar(float farClip) { m_PerspectiveFar = farClip; }
+
+		void SetOrthographic(float size, float nearClip, float farClip);
 		float GetOrthographicSize() const { return m_OrthographicSize; }
 		void SetOrthographicSize(float size) { m_OrthographicSize = size; RecalculateProjection(); }
+		float GetOrthographicNear() const { return m_OrthographicNear; }
+		void SetOrthographicNear(float nearClip) { m_OrthographicNear = nearClip; RecalculateProjection(); }
+		float GetOrthographicFar() const { return m_OrthographicFar; }
+		void SetOrthographicFar(float farClip) { m_OrthographicFar = farClip; }
+
+		ProjectionType GetProjectionType() const { return m_ProjectionType; }
+		void SetProjectionType(ProjectionType projection)
+		{
+			m_ProjectionType = projection;
+			RecalculateProjection();
+		}
 
 	private:
 		void RecalculateProjection();
 
 	private:
+		ProjectionType m_ProjectionType = ProjectionType::Orthographic;
+
+		float m_PerspectiveFOV = 45.0f;
+		float m_PerspectiveNear = 0.01f;
+		float m_PerspectiveFar = 1000.0f;
+
 		float m_OrthographicSize = 10.0f;
 		float m_OrthographicNear = -1.0f;
 		float m_OrthographicFar = 1.0f;
