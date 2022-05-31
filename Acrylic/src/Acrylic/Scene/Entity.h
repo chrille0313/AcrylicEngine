3ba806a85de2c047f9e566421aa2cc780166dd52
Christian <chrille_0313@hotmail.com>
2022-06-01 00:33:50 +0200

    Update Entity.h

--- Acrylic/src/Acrylic/Scene/Entity.h
+++ Acrylic/src/Acrylic/Scene/Entity.h
@@ -16,11 +16,16 @@ namespace Acrylic {
 		virtual ~Entity() = default;
 
 		operator bool() const { return m_EntityHandle != entt::null; }
+		operator entt::entity() const { return  m_EntityHandle; }
+		operator uint32_t() const { return  (uint32_t)m_EntityHandle; }
+
+		bool operator==(const Entity& other) const { return m_EntityHandle == other.m_EntityHandle && m_Scene == other.m_Scene; }
+		bool operator!=(const Entity& other) const { return !(*this == other); }
 
 		template<typename T>
 		bool HasComponent()
 		{
-			m_Scene->m_Registry.any_of<T>(m_EntityHandle);
+			return m_Scene->m_Registry.any_of<T>(m_EntityHandle);
 		}
 
 		template<typename T>
